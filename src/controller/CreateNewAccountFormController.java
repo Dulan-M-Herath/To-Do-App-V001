85c04eec8759a6c2232be1593a2ea31cf96e4a4d
Dulan Maheshitha Herath <dulanmherath@outlook.com>
2023-06-22 23:04:29 +0530

    JDBC part and singleton applied.

--- src/controller/CreateNewAccountFormController.java
+++ src/controller/CreateNewAccountFormController.java
@@ -1,17 +1,29 @@
 package controller;
 
+import db.DBConnection;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.sql.Connection;
 
 public class CreateNewAccountFormController {
     public PasswordField txtNewPassword;
     public PasswordField txtConfirmPassword;
     public Label lblPasswordDoesntMatch1;
     public Label lblPasswordDoesntMatch2;
+    public AnchorPane rootCreateNewAccountForm;
+    public TextField txtUsername;
+    public TextField txtEmail;
+    public Button btnRegister;
 
     public void initialize(){
+
         passwordDoesntMatchLable(false);
+        displayFields(true);
     }
 
     public void btnRegisterOnAction(ActionEvent actionEvent) {
@@ -32,5 +44,22 @@ public class CreateNewAccountFormController {
     public void passwordDoesntMatchLable(boolean state){
         lblPasswordDoesntMatch1.setVisible(state);
         lblPasswordDoesntMatch2.setVisible(state);
+    }
+
+    public void btnAddNewUser(ActionEvent actionEvent) {
+        displayFields(false);
+        txtUsername.requestFocus();
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        System.out.println(connection);
+    }
+
+    public void displayFields(boolean state){
+        txtUsername.setDisable(state);
+        txtEmail.setDisable(state);
+        txtNewPassword.setDisable(state);
+        txtConfirmPassword.setDisable(state);
+        btnRegister.setDisable(state);
+
     }
 }
